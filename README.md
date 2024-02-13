9dbd07d8714103f73567eb6247ffafb700e7c95e
yadyad <yadhup.krishna@gmail.com>
2024-02-13 14:57:55 +0100

    First commit after basic changes

--- README.md
+++
@@ -1,2 +0,0 @@
-# ABA
-Furhat robot experiment
--- Template/src/main/kotlin/furhatos/app/templateadvancedskill/utils/events.kt
+++
@@ -1,7 +0,0 @@
-package furhatos.app.templateadvancedskill.utils
-
-import furhatos.event.Event
-
-class RandomHeadEvent : Event()
-class NoUsersPresentEvent : Event()
-
